faf63736a823c8638a0040c7a18631674398bcb1
Anton Sofronov <231.334@mail.ru>
2021-09-23 20:51:30 +0300

    Добавил комментарии к работе

--- src/Laba/Primes.java
+++ src/Laba/Primes.java
@@ -2,20 +2,21 @@ package Laba;
 
 public class Primes {
     public static void main(String[] args) {
-        for (int i=0; i != 100; i++){
+        for (int i=0; i != 100; i++){//Цикл с числами на проверку
             if (isPrimes(i) == true){
                 System.out.println(i);
             }
         }
     }
 
-    public static boolean isPrimes(int n) {
+    public static boolean isPrimes(int n) { // Функция с логическим выводом для понимая какое число простое а какое нет
         double sqrt = Math.sqrt(n);
-        if(n % 2 == 0){
+        if(n % 2 == 0){ //Исключаем попдание всех четных чисел как они по определению не могут быть простыми
             return false;
         }
-        for (int i = 3; i <= n; i += 2) {
-            if ((i > sqrt)) {
+        for (int i = 3; i <= n; i += 2) { //Исключаем попадания всех четных чисел как они по определению не могут быть простыми
+            if ((i > sqrt)) {//Проверка еще одного правила делителями могут быть только те простые числа,
+                                // которые не превосходят квадратного корня из проверяемого числа
                 return true;
             }
             if (n % i == 0) {
